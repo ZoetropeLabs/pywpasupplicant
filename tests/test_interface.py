e1fb1fdea0ed85e2252d4030e8ebe46d116a8d50
Michael Boulton <boulton@zoetrope.io>
2016-12-06 14:04:42 +0000

    Test

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,6 +2,6 @@ from pywpasupplicant.wpa_ctrl import DirectWPAInterface
 
 
 def test_interface():
-    iface = DirectWPAInterface()
+    iface = DirectWPAInterface("/var/run/wpa_supplicant/wlx98ded014d040")
 
     iface.ctrl_request("SCAN")
